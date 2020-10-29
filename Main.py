4bd54cf8c6c4547d98c9cedd6952a36fdfd25bb5
KorolKirill <71098249+Hehehe3plx@users.noreply.github.com>
2020-10-29 12:06:46 +0200

    Bubble sort

--- Main.py
+++ Main.py
@@ -1,5 +1,10 @@
 import random
+import time
 
+start = time.time()
+
+
+# Зассекает время
 
 def make_consistent_list(list_range):
     empty_list = []
@@ -26,6 +31,23 @@ random_list_100 = make_random_list(100)
 random_list_1k = make_random_list(1_000)
 random_list_10k = make_random_list(10_000)
 
+
 # задаем наши списки
 
+def bubble_sort(given_list):
+    list_len = len(given_list)
+    for x in range(list_len):
+        for y in range(list_len - 1):
+            if given_list[y] > given_list[y + 1]:
+                save_element = given_list[y]
+                given_list[y] = given_list[y + 1]
+                given_list[y + 1] = save_element
+    return given_list
+
+
 print(random_list_100)
+
+end = time.time()
+print(end - start)
+
+# Пишет сколько было затраченно времени в секундах, !Должно быть в самом конце
