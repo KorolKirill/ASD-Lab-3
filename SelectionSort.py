f1b48f236d18386ef54b3d430e3790401ed03b8b
NikitaBukreyev <73642125+NikitaBukreyev@users.noreply.github.com>
2020-11-03 16:39:12 +0200

    Add files via upload

---
+++ SelectionSort.py
@@ -0,0 +1,23 @@
+import time
+
+def SelectionSort( itemsList ):
+    start = time.time()
+
+    n = len( itemsList )
+    for i in range( n - 1 ): 
+        minValueIndex = i
+
+        for j in range( i + 1, n ):
+            if itemsList[j] < itemsList[minValueIndex] :
+                minValueIndex = j
+
+        if minValueIndex != i :
+            temp = itemsList[i]
+            itemsList[i] = itemsList[minValueIndex]
+            itemsList[minValueIndex] = temp
+
+    end = time.time()
+    estimatedTime = "Selection sort for " + str(n) + " elements took " + str(end - start) + " seconds"
+
+    return estimatedTime
+
